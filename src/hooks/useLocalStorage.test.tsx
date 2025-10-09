6606c100e30c7b056d570264d5cda33a241f97f8
Samuel Stephen <samuelstephen1234@gmail.com>
2025-10-09 09:15:46 +0530

    P5S14: Test Loading Existing Data from localStorage

--- src/hooks/useLocalStorage.test.tsx
+++ src/hooks/useLocalStorage.test.tsx
@@ -1,8 +1,24 @@
 import { renderHook } from '@testing-library/react';
 import useLocalStorage from './useLocalStorage';
-import { use } from 'react';
+import { use, act } from 'react';
 
-test('initializes with default value', () => {
+//Step 12: Test Initializing useLocalStorage with Default Value
+test('P5S12: initializes with default value', () => {
     const { result } = renderHook(() => useLocalStorage<Todo[]>('todo', []))
     expect(result.current[0]).toEqual([]);
+});
+
+//Step 13: Test Loading Existing Data from localStorage
+test('P5S13: loads existing data from localStorage', () => {
+    window.localStorage.setItem('todos', JSON.stringify([{ id: '1' }]));
+    const { result } = renderHook(() => useLocalStorage<Todo[]>('todos', []))
+    expect(result.current[0]).toEqual([{ id: '1' }]);
+});
+
+test('P5S14: saves updates to localStorage', () => {
+    const { result } = renderHook(() => useLocalStorage<Todo[]>('todos', []));
+    act(() => {
+        result.current[1]([{ id: '2' }]);
+    });
+    expect(window.localStorage.getItem('todos')).toBe(JSON.stringify([{ id: '2' }]));
 });
